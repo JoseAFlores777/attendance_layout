899ee7a09fc6c64bbc039ff6e8995e290fec5e98
JoseAFlores777 <65737946+JoseAFlores777@users.noreply.github.com>
2021-01-14 19:30:25 -0600

    Al menos ya hace las pausas, pero falta hacer el clearInterval

--- User_layout/index.js
+++ User_layout/index.js
@@ -21,112 +21,171 @@ $(document).ready(function () {
  
   $(".date_crono").html(`<h5 class="text-muted"> ${event.toLocaleDateString('en-US', options)} </h5>`);
   
+
+
   //Cronómetro
  
   let isPaused = false;
+  let pausas = [];
+
+  class pausa  {
+    hora= 0;
+    minuto= 0;
+    segundo= 0;
+    des = '';
+    constructor() { 
+      this.hora = 0;
+      this.minuto = 0;
+      this.segundo = 0;
+    }
+
+    set setHora(hora) { 
+      this.hora = hora;
+    }
+    set setMinuto(minuto) { 
+      this.minuto = minuto;
+    }
+    set setSegundo(segundo) { 
+      this.segundo = segundo;
+    }
+
+    set setDes(des) { 
+      this.des = des;
+    }
+
+    get getDuracion() { 
+      return `${this.hora < 10 ? '0' + this.hora : this.hora}:${this.minuto < 10 ? '0' + this.minuto : this.minuto}:${this.segundo < 10 ? '0' + this.segundo : this.segundo}`;
+    }
+
+    get getDes() { 
+      return this.des;
+    }
+    
+  }
+
+
+
   let tiempo_trabajo = {
     hora: 0,
     minuto: 0,
     segundo: 0
   };
 
-  let tiempo_pausa = {
-    hora: 0,
-    minuto: 0,
-    segundo: 0
-};
-
-let 
-running_time = null;
 
 $("#btn_start").click(function(){
-    // if ($(this).text() == 'Start' || $(this).text() == 'Restart')
-    // {
-    //     $(this).text('Pause');
+
   $(this).parent().addClass('active-btn');    
   $(this).hide("true");   
   $(".btn2_crono").show("true");
 
 
-  Cronometro(tiempo_trabajo.hora, tiempo_trabajo.minuto, tiempo_trabajo.hora);
-        
-        // running_time = setInterval(function(){
-        //     // Segundos
-        //     p_seg++;
-        //   if (p_seg >= 60)
-            
-        //     {
-        //     p_seg = 0;
-        //         p_min++;
-        //     }      
+  Cronometro_Trabajo(tiempo_trabajo.hora, tiempo_trabajo.minuto, tiempo_trabajo.segundo);
 
-        //     // Minutos
-        //   if (p_min >= 60)
-            
-        //     {
-        //         p_min = 0;
-        //         p_hr++;
-        //     }
-        //   console.log(p_seg);
-        //    return  $("#crono").text(`${p_hr < 10 ? '0' + p_hr : p_hr}:${p_min < 10 ? '0' + p_min : p_min}:${p_seg < 10 ? '0' + p_seg : p_seg}`);
-
-        // }, 1000);
-  
-  
-  
-    // }
-    // else 
-    // {
-    //     $(this).text('Restart');
-    //     $(this).parent().removeClass('active-btn');
-    //     clearInterval(running_time);
-    // }
 });
   
   $('#btn_pause').click(function () {    
-    isPaused = true;
-    $(this).text('Keep working');
-    $(this).removeClass('btn-warning');
-    $(this).addClass('btn-success');
-    // $('#btn_pause').
-    isPaused = false;
+    if ($(this).text() == ' Pause') {
+      isPaused = true;
+      $(this).text('Keep working');
+      $(this).removeClass('btn-warning');
+      $(this).addClass('btn-success');
+      pausa_tmp =  new pausa();
+      pausas.push(pausa_tmp);
+      Cronometro_Pausas(pausa_tmp);
+      
+    } else if ($(this).text() == 'Keep working') {
+      
+      isPaused = false;
+      $(this).text(' Pause');
+      $(this).removeClass('btn-success');
+      $(this).addClass('btn-warning');
+      // pausa_tmp =  new pausa();
+      // pausas.push(pausa_tmp);
+      // Cronometro_Pausas(pausa_tmp);
+    }
 
-   Cronometro(tiempo_pausa.hora, tiempo_pausa.minuto, tiempo_pausa.hora);
-});
+  });
+
+
   
-  
-  
-  const Cronometro = (p_seg,p_min,p_hr) => { 
+  const Cronometro_Trabajo = (p_hr,p_min,p_seg) => { 
 
 
     setInterval(function () {
-      if (!isPaused) {
-        
-        // Segundos
-        p_seg++;
-      if (p_seg >= 60)
-             {
-        p_seg = 0;
-            p_min++;
-        }      
+if (!isPaused) {
   
-        // Minutos
-      if (p_min >= 60)
-        
-        {
-            p_min = 0;
-            p_hr++;
-        }
-        console.log({ p_seg,p_min,p_hr });
+  // Segundos
   
+  p_seg++;
   
-       return  $("#crono").text(`${p_hr < 10 ? '0' + p_hr : p_hr}:${p_min < 10 ? '0' + p_min : p_min}:${p_seg < 10 ? '0' + p_seg : p_seg}`);
-      }
+  if (p_seg >= 60) {
+    p_seg = 0;
+    p_min++;
+  }
+  // Minutos
+  if (p_min >= 60) {
 
-  }, 1000);
+    p_min = 0;
+    p_hr++;
+  }
+
+tiempo_trabajo.segundo = p_seg;
+tiempo_trabajo.minuto = p_min;
+tiempo_trabajo.hora = p_hr;
+
+  // console.log(p_seg,p_min,p_hr);
+  // console.log(tiempo_trabajo);
+
+  return $("#crono").text(`${p_hr < 10 ? '0' + p_hr : p_hr}:${p_min < 10 ? '0' + p_min : p_min}:${p_seg < 10 ? '0' + p_seg : p_seg}`);
+}
+
+    }, 1000);
+    
 
     
+    
   }
+
+
+  const Cronometro_Pausas = (pausa) => { 
+    p_hr = 0;
+    p_min = 0;
+    p_seg = 0;
+    let tiempo =  setInterval(function () {
+if (isPaused) {
+  // Segundos
+
+  p_seg++;
+  
+  if (p_seg >= 60) {
+    p_seg = 0;
+    p_min++;
+  }
+  // Minutos
+  if (p_min >= 60) {
+
+    p_min = 0;
+    p_hr++;
+  }
+
+pausa.setSegundo = p_seg;
+pausa.setMinuto = p_min;
+pausa.setHora = p_hr;
+
+  // console.log(p_seg,p_min,p_hr);
+  // console.log(tiempo_trabajo);
+
+  return $("#crono").text(`${p_hr < 10 ? '0' + p_hr : p_hr}:${p_min < 10 ? '0' + p_min : p_min}:${p_seg < 10 ? '0' + p_seg : p_seg}`);
+}
+
+    }, 1000);
+    
+
+  
+    
+  }
+
+ 
   
   
   
